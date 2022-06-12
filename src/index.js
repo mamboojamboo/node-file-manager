758f6b207730090bdeede251aaa6187648826f83
Mikhail Afonin <afoninmv@gmail.com>
2022-06-12 15:15:23 +0300

    feat: add start and end messages, readline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('start!');
+import readline from 'readline'
+
+import {user, startMessageInConsole, endMessageInConsole} from './process/user.js';
+import {toConsole} from './console/toConsole.js';
+
+
+toConsole(startMessageInConsole);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (input) => {
+	toConsole(`Received: ${input}`);
+  });
+
+rl.on('SIGINT', () => {
+	toConsole(endMessageInConsole);
+	rl.close();
+})
+
+rl.on('SIGTSTP', () => {
+	toConsole(endMessageInConsole);
+	rl.close();
+})
---
+++ src/intl/index.js
@@ -0,0 +1,6 @@
+export const INTL = {
+	MESSAGES: {
+		START_APP: 'Welcome to the File Manager,',
+		EXIT_APP: 'Thank you for using File Manager,'
+	}
+}
